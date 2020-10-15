398c86a822b267345b8178de63ba2c19355510ca
Gullit <plamedi@Plamedis-MacBook-Pro.local>
2020-10-14 22:45:01 -0700

    Fim implementacao LAB03

---
+++ LAB03/LAB03AritmeticaApp/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB03AritmeticaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"\n\t\tData: {DateTime.Now}\t\tLAB03");
+            ServiceAritmetica service = new ServiceAritmetica();
+            int opcao;
+            bool opcaoValida = true;
+            string resposta = "Sim";
+            string subline = "______________________________________________________________________________________";
+            do
+            {
+                Console.Write("\n\t\tEscolha uma opção do menu: \n" +
+                    "\n\t\t\t1 - Achar intervado de número" +
+                    "\n\t\t\t2 - Extrair cada dígito de um número" +
+                    "\n\t\t\t3 - Calcular a função: f(x,y) = (((x^2) + 3x + (y^2)) / ((xy) - 5y - 3x + 15))" +
+                    "\n\t\t\t4 - Calcular a série: (x^25)/1 - (x^24)/2 + (x^23)/3 - (x^22)/4 + ... + (x)/25" +
+                    "\n\t\t\t5 - Calcular uma expressão da seguinte forma: s = 1/1 - 2/4 + 3/9 - 4/16 + 5/25 - 6/36 + ... - 10/100" +
+                    $"\n\t\t{subline}\n" +
+                    "\n\t\t\t\tQual sua opção?: ");
+
+                opcao = Int32.Parse(Console.ReadLine());
+                Console.WriteLine($"\t\t{subline}\n");
+
+                switch(opcao)
+                {
+                    case 1:
+                        {
+                            Console.Write("\t\tInforme o número: ");
+                            double numero = Double.Parse(Console.ReadLine());
+
+                            Console.Write("\n\t\tInforme o valor da extremidade A: ");
+                            double extremidadeA = Double.Parse(Console.ReadLine());
+                            Console.Write("\t\tInforme o valor da extremidade B: ");
+                            double extremidadeB = Double.Parse(Console.ReadLine());
+
+                            Console.Write("\n\t\tInforme o valor da extremidade C: ");
+                            double extremidadeC = Double.Parse(Console.ReadLine());
+                            Console.Write("\t\tInforme o valor da extremidade D: ");
+                            double extremidadeD = Double.Parse(Console.ReadLine());
+
+                            string intervalo = service.AcharIntervadoNumero(numero, (extremidadeA, extremidadeB, extremidadeC, extremidadeD));
+
+                            Console.WriteLine($"\n\t\tO número {numero}" + (intervalo != string.Empty ?
+                                $" pertence ao intervalo: {intervalo}" :
+                                $" não pertence a nenhum dos intervalos: [{extremidadeA},{extremidadeB}] & [{extremidadeC},{extremidadeD}]"));
+
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            Console.Write("\n\t\tInforme o Número: ");
+                            int numero = Int32.Parse(Console.ReadLine());
+
+                            if(numero < 1000 || numero > 9999)
+                            {
+                                Console.Write("\n\t\t\t\tNúmero inválido!");
+                                break;
+                            }
+
+                            var (digito1, digito2, digito3, digito4) = service.ExtrairDigitoNumero(numero);
+
+                            Console.WriteLine($"\n\t\tOs dígitos do número fornecido são: {digito1}, {digito2}, {digito3} e {digito4}");
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            Console.WriteLine("\t\tCálculo da função: \n\t\tf(x,y) = (((x^2) + 3x + (y^2)) / ((xy) - 5y - 3x + 15))\n");
+
+                            double f;
+                            for (int x = 1; x <= 5; x++)
+                            {
+                                for (int y = 0; y <= 5; y++)
+                                {
+                                    f = (Math.Pow(x, 2)) + 3 * x + (Math.Pow(y, 2));
+                                    Console.WriteLine($"\t\tf({x},{y}) = {f}");
+                                }
+                                Console.WriteLine();
+                            } 
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            string mensagem = string.Empty;
+                            Console.Write("\n\t\tInforme um número: ");
+
+                            double numero = Double.Parse( Console.ReadLine());
+
+                            IEnumerable<(int contador, int m, int exp, double soma, double parcela, double somatorio)> results = service.CalcularSomaTermosSerie(numero);
+
+
+                            foreach(var (contador, m, exp, soma, parcela, somatorio) in results)
+                            {
+                                if (contador == 1)
+                                    mensagem += $"\t\t({numero}^{exp}) / {contador} = {parcela}\n\n";
+                                else
+                                    mensagem += $"\t\t({numero}^{exp}) / {contador} = {parcela}\n\t\t{somatorio} " + (m == 1 ? "+" : "-") + $" {parcela} = {soma}\n\n";
+                            }
+
+                            Console.WriteLine($"\n\t\tResultado:\n\n {mensagem}");
+
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            double result = service.CalcularExpressao();
+                            Console.WriteLine($"\n\t\tA soma dos 10 termos da série é: {result}");
+                            
+                            break;
+                        }
+
+                    default:
+                        {
+                            opcaoValida = false;
+                            Console.Write("\t\t\t\tOpção Inválida! Tente outra vez!");
+                            break;
+                        }
+
+                }
+
+                Console.WriteLine($"\n\t\t{subline}");
+
+                if(opcaoValida)
+                {
+                    Console.Write("\n\t\tDeseja continuar?, digite \"Sim\" para prosseguir: ");
+                    resposta = Console.ReadLine();
+                    Console.Clear();
+                }
+                
+
+            } while(string.Equals(resposta, "sim", StringComparison.CurrentCultureIgnoreCase) ||
+                    string.Equals(resposta, "s", StringComparison.CurrentCultureIgnoreCase));
+
+            Console.WriteLine($"\t\t{subline}");
+            Console.WriteLine();
+        }
+
+
+        
+    }
+}
---
+++ LAB03/LAB03AritmeticaApp/ServiceAritmetica.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB03AritmeticaApp
+{
+    public class ServiceAritmetica
+    {
+        public string AcharIntervadoNumero(double numero, (double a, double b, double c, double d) extremidade)
+        {
+            double extremidadeA = extremidade.a < extremidade.b ? extremidade.a : extremidade.b;
+            double extremidadeB = extremidade.b > extremidade.a ? extremidade.b : extremidade.a;
+            double extremidadeC = extremidade.c < extremidade.d ? extremidade.c : extremidade.d;
+            double extremidadeD = extremidade.d > extremidade.c ? extremidade.d : extremidade.c;
+
+            if((numero < extremidadeA && numero < extremidadeC) || (numero > extremidadeB && numero > extremidadeD))
+                return string.Empty;
+            else
+            {
+                if((numero >= extremidadeA) && (numero <= extremidadeB) && (numero >= extremidadeC) && (numero <= extremidadeD))
+                    return $"[{extremidade.a},{extremidade.b}] & [{extremidade.c},{extremidade.d}]";
+                else
+                {
+                    if((numero >= extremidadeA) && (numero <= extremidadeB))
+                        return $"[{extremidade.a},{extremidade.b}]";
+                    else
+                        return $"[{extremidade.c},{extremidade.d}]";
+                }
+            }
+        }
+        public (int digito1, int digito2, int digito3, int digito4) ExtrairDigitoNumero(int numero)
+        {
+            int digito1 = (numero / 1000) % 10;
+            int digito2 = (numero / 100) % 10;
+            int digito3 = (numero / 10) % 10;
+            int digito4 = (numero % 10);
+
+            return (digito1, digito2, digito3, digito4);
+        }
+
+        public IEnumerable<(int contador, int m, int exp, double soma, double parcela, double somatorio)> CalcularSomaTermosSerie(double numero)
+        {
+            var results = new List<(int contador, int m, int exp, double soma, double parcela, double somatorio)>();
+            int contador = 1;
+            int m = 1;
+            int exp = 25;
+            double soma = 0;
+            double parcela;
+            double somatorio = 0;
+
+            do
+            {
+                parcela = ((Math.Pow(numero, exp)) / contador);
+                soma += parcela;
+                if (contador == 1)
+                {
+                    results.Add((contador, m, exp, soma, parcela, somatorio));
+                    somatorio = soma;
+                }
+                else
+                {
+                    results.Add((contador, m, exp, soma, parcela, somatorio));
+                    somatorio = soma;
+                }
+
+                m *= -1;
+                exp--;
+                contador++;
+
+            } while (contador <= 25);
+
+            return results;
+
+        }
+
+        public double CalcularExpressao()
+        {
+            int num = 1;
+            int m = 1;
+            int cont = 0;
+            double result = 0;
+            double parcela;
+
+            do
+            {
+                parcela = (num / (Math.Pow(num, 2))) * m;
+                m *= -1;
+                result += parcela;
+                num += 1;
+                cont++;
+            } while (num <= 10);
+
+            return result;
+        }
+    }
+}
